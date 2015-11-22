9d89e4e6941a2de4cb514dc7cc15f3eb23decf01
JerryChia <jiayzhou_study@163.com>
2015-11-22 11:13:22 +0800

    add filewatch and setautorun

---
+++ DBFConvert/Src/Common.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBFConvert.Src
+{
+    public static class Common
+    {
+        // SetAutoRun(Application.ExecutablePath, true);  //设置自动启动当前程序
+        // SetAutoRun(Application.ExecutablePath, false);  //取消自动启动 
+
+        /// <summary>
+        /// 设置开机自动启动
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="isAutoRun"></param>
+        public static void SetAutoRun(string fileName, bool isAutoRun)
+        {
+            RegistryKey reg = null;
+            try
+            {
+                if (!System.IO.File.Exists(fileName))
+                    throw new Exception("该文件不存在!");
+                String name = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
+                reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                if (reg == null)
+                    reg = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                if (isAutoRun)
+                    reg.SetValue(name, fileName);
+                else
+                    reg.SetValue(name, false);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+            finally
+            {
+                if (reg != null)
+                    reg.Close();
+            }
+        }
+    }
+}
---
+++ DBFConvert/Src/FileWatchHelper.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DBFConvert.Src
+{
+    public class FileWatchHelper
+    {
+
+        public static System.IO.FileSystemWatcher m_Watcher = new System.IO.FileSystemWatcher();
+
+        private static string fileWatchPath = @"D:\tianchi\";
+        private static string fileWatchFile = "gd_line_desc.txt";
+        private static bool _isWatch = false;
+
+
+        private static FileWatchHelper fileWatchHelper = null;
+        public static FileWatchHelper getInstance(string filepath,string filter) 
+        {
+            if (fileWatchHelper == null)
+                fileWatchHelper = new FileWatchHelper();
+
+            fileWatchPath = filepath;
+            fileWatchFile = filter;
+            InitWatchHelper();
+
+            return fileWatchHelper;
+        }
+
+        private static void InitWatchHelper() 
+        {
+            m_Watcher.Path = fileWatchPath;
+            m_Watcher.Filter = fileWatchFile;
+            m_Watcher.NotifyFilter = NotifyFilters.LastAccess |
+                         NotifyFilters.LastWrite |
+                         NotifyFilters.FileName |
+                         NotifyFilters.DirectoryName;
+            m_Watcher.Changed += new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Created += new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Deleted += new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Renamed += new RenamedEventHandler(m_Watcher_Resumed);
+            m_Watcher.IncludeSubdirectories = false;//包含子目录 
+        }
+
+        /// <summary>
+        /// 监控是否正在运行
+        /// </summary>
+        public bool IsWatch
+        {
+            get
+            {
+                return _isWatch;
+            }
+        }
+
+        /// <summary>
+        /// 开始监视
+        /// </summary>
+        public void  EnableWatch()
+        {
+            _isWatch = true;
+            m_Watcher.EnableRaisingEvents = true;//开始监视
+        }
+        /// <summary>
+        /// 结束监视
+        /// </summary>
+        public void DisableWatch()
+        {
+            _isWatch = false;
+            m_Watcher.EnableRaisingEvents = false;//结束监视
+        }
+
+        /// <summary>
+        /// 关闭监听器
+        /// </summary>
+        public void Close()
+        {
+            _isWatch = false;
+            m_Watcher.Changed -= new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Created -= new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Deleted -= new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Renamed -= new RenamedEventHandler(m_Watcher_Resumed);
+            m_Watcher.EnableRaisingEvents = false;
+            m_Watcher = null;
+        }
+
+        public delegate void FileChangedEvent(System.IO.FileSystemEventArgs e);
+        static FileChangedEvent fileChangedEvent;
+
+        /// <summary>
+        /// 监听事件触发的方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void m_Watcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            fileChangedEvent(e);
+            /////这里写你监视的函数
+        }
+        /// <summary>
+        /// 文件或目录重命名时触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void m_Watcher_Resumed(object sender, RenamedEventArgs e)
+        {
+            //这里不用写
+        }
+
+    }
+}
