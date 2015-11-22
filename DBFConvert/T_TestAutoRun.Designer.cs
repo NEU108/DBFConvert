14e9b20ec3601cdcc12e3f8f81a4cb65814b879d
JerryChia <jiayzhou_study@163.com>
2015-11-22 18:38:17 +0800

    jerrychia

---
+++ DBFConvert/T_TestAutoRun.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DBFConvert
+{
+    partial class T_TestAutoRun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.testnotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.testcontextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testcontextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // testnotifyIcon
+            // 
+            this.testnotifyIcon.Text = "notifyIcon1";
+            this.testnotifyIcon.Visible = true;
+            this.testnotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.testnotifyIcon_MouseDoubleClick);
+            // 
+            // testcontextMenuStrip
+            // 
+            this.testcontextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.testcontextMenuStrip.Name = "testcontextMenuStrip";
+            this.testcontextMenuStrip.Size = new System.Drawing.Size(101, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.showToolStripMenuItem.Text = "显示";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.hideToolStripMenuItem.Text = "隐藏";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "退出";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // T_TestAutoRun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.MaximizeBox = false;
+            this.Name = "T_TestAutoRun";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T_TestAutoRun";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.T_TestAutoRun_FormClosing);
+            this.Load += new System.EventHandler(this.T_TestAutoRun_Load);
+            this.SizeChanged += new System.EventHandler(this.T_TestAutoRun_SizeChanged);
+            this.testcontextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon testnotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip testcontextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
