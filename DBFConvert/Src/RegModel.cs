269af148ee83e0d04d0851922b0f63b7315b52d0
JerryChia <jiayzhou_study@163.com>
2015-11-24 05:26:08 +0800

    commit for jerrychia ver1.0

---
+++ DBFConvert/Src/RegModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBFConvert.Src
+{
+    public class RegModel
+    {
+        public string _installTime = "";
+        public string _code = "";
+        public string _activeTime = "";
+        public string _type = "";
+        public string _IncName = "";
+        public string _key = "";
+    }
+}
